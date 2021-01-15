cb8a4fbbeab411d874bc0e7e3a1044bb1ad8815e
Saiful Islam <saifulislampp23@gmail.com>
2021-01-15 23:54:59 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/single_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.single_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:single_chat/view/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xFFFF75431),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
